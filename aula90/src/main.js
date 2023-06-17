4dfcbd80d82a380887fe73758f0022d925c4b1b5
Renan Mendes <renanmendes421@icloud.com>
2023-06-17 09:35:19 -0300

    Aula90 - Update

--- aula90/src/main.js
+++ aula90/src/main.js
@@ -1,4 +1,9 @@
-import GeraCPF from '.modules/GeraCPF';
+import GeraCPF from './modules/GeraCPF';
 
 import './assets/css/style.css';
 
+(function() {
+    const gera = new GeraCPF();
+    const cpfGerado = document.querySelector('.cpf-gerado');
+    cpfGerado.innerHTML = gera.geraNovoCpf()
+})();
